a888817f51ec7d9fbc386ae5faa5c441a3e18a39
Mikeexe2 <splendens0106@gmail.com>
2024-08-01 22:29:08 +0800

    offset removed

--- js/stats.js
+++ js/stats.js
@@ -4,7 +4,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         e.preventDefault();
 
         const target = this.getAttribute('href');
-        const offset = 60;
+        const offset = 0;
         const targetElement = document.querySelector(target);
 
         if (targetElement) {
@@ -50,7 +50,7 @@ document.addEventListener("visibilitychange", function () {
         document.title = "(/≧▽≦/) Welcome back!";
         titleTime = setTimeout(function () {
             document.title = originalTitle;
-        }, 2000);
+        }, 1000);
     }
 });
 
@@ -77,57 +77,3 @@ function getDayAbbreviation(day) {
 
 updateClock();
 setInterval(updateClock, 1000);
-
-// 0x40 hues animation??
-const hues = Array.from({ length: 128 }, () => Math.floor(Math.random() * 3600 / 10) * 10);
-
-const background = document.getElementById('background');
-const randomimg = document.getElementById('randomimg');
-const toggleAnimation = document.getElementById('toggleAnimation');
-
-let animationTimeout;
-
-function setRandomBackgroundColor(hue) {
-    background.style.backgroundColor = `hsla(${hue}, 80%, 70%, 1.0)`;
-}
-
-function setRandomImage() {
-    const randomIndex = Math.floor(Math.random() * 296) + 1;
-    randomimg.src = `images/image${randomIndex}.png`;
-
-    const animation = ['anim-shake', 'anim-blur-left', 'anim-blur-right', 'anim-blur-top', 'anim-blur-bottom'];
-
-    const anim = animation[Math.floor(Math.random() * animation.length)];
-
-    randomimg.className = '';
-
-    randomimg.classList.add(anim);
-}
-
-function getRandomInterval(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-function animateHues() {
-    function animate() {
-        if (toggleAnimation.checked) {
-            const hue = hues[Math.floor(Math.random() * hues.length)];
-            setRandomBackgroundColor(hue);
-            setRandomImage();
-
-            const interval = getRandomInterval(0.5, 1.2) * 1000;
-            animationTimeout = setTimeout(animate, interval);
-        }
-    }
-
-    animate();
-}
-
-toggleAnimation.addEventListener('change', () => {
-    if (toggleAnimation.checked) {
-        animateHues();
-    } else {
-        clearTimeout(animationTimeout);
-    }
-});
-
